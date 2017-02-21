6aa90ee81132172211b9960812946e5730721046
Angela Conant <goteampeach@gmail.com>
2017-02-20 16:41:17 -0800

    Now with color and multiple objects

--- Sphere.py
+++ Sphere.py
@@ -3,15 +3,16 @@ from Ray import Ray
 import math
 
 class Sphere:
-    def __init__(self, coord, r):
+    def __init__(self, coord, r, color):
         self.pos = coord
         self.radius = r
+        self.color = color
 
-    def does_ray_intersect(self, ray):
+    def ray_intersect(self, ray):
         # vector between camera and sphere pos
-        L = Coord(ray.origin.x-self.pos.x, 
-                  ray.origin.y-self.pos.y, 
-                  ray.origin.z-self.pos.z)
+        L = Coord(self.pos.x - ray.origin.x, 
+                  self.pos.y - ray.origin.y, 
+                  self.pos.z - ray.origin.z)
         b = 2.0 * ray.unitvector.x * (L.x) + 2.0 * ray.unitvector.y * (L.y) + \
             2.0 * ray.unitvector.z * (L.z)
         c = self.pos.x**2 + self.pos.y**2 + self.pos.z**2 + \
@@ -21,10 +22,12 @@ class Sphere:
         d = b**2 - 4*c
         # If the discriminate is negative, there is no intersection
         if d < 0:
-            return False
+            return 0.0
         else:
-            return True
-        '''
-        t0 = (-b - math.sqrt(d)) / 2
-        t1 = (-b + math.sqrt(d)) / 2
-        '''
+            t0 = (-b - math.sqrt(d)) / 2.0
+            if t0 >= 0.0: 
+                return t0
+            t1 = (-b + math.sqrt(d)) / 2.0
+            if t1 >= 0:
+                return t1
+            return 0.0
--- raytracer.py
+++ raytracer.py
@@ -3,8 +3,26 @@ from Coord import Coord
 from Ray import Ray
 from Sphere import Sphere
 
+def raytrace(ray, objects):
+    last_intersect = None
+    last_dist = None
+    for x in objects:
+        dist = x.ray_intersect(ray)
+        # if the ray intersects and is closer
+        if dist != 0 and \
+            (last_intersect is None or dist < last_dist):
+            last_intersect = x
+            last_dist = dist
+    if last_intersect:
+        pixels.append( last_intersect.color )
+    else:
+        pixels.append( (0, 0, 0) )
+    return
+
 camera = Coord(0, 0, 0)
-sphere = Sphere( Coord(0, 0, 10), 0.5 )
+objects = []
+objects.append( Sphere( Coord(0, 0, 10), 0.5, (255, 0, 0) ) )
+objects.append( Sphere( Coord(0.5, 0, 16), 1, (0, 255, 0) ) )
 
 width = 200
 height = 150
@@ -16,10 +34,7 @@ for y in range(height):
     xpos = -width / 50 / 2.0
     for x in range(width):
         ray = Ray(camera, Coord(xpos, ypos, 6))
-        if sphere.does_ray_intersect(ray):
-            pixels.append( (255, 255, 255) )
-        else:
-            pixels.append( (0, 0, 0) )
+        raytrace(ray, objects)
         xpos += 0.02
     ypos += 0.02
 
